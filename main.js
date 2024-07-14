d3a3a2fecd625dc797bcc59455ec6d46dafd65bb
Gman <omar5mahmoud3ahmed6@gmail.com>
2024-07-14 15:13:42 +0200

    make nav btn event from click to 'onhold'

--- main.js
+++ main.js
@@ -1,25 +1,81 @@
+// const nav = {
+//     box : document.getElementById("pagesBtnsBox"),
+//     goR : document.getElementById("GoRight"),
+//     goL : document.getElementById("Goleft"),
+//     runder : function(){
+
+//         this.goR.addEventListener("mousedown",
+//             function () {
+//                 nav.box.scrollBy(20,0)
+//             }
+//         );
+
+
+
+
+//         this.goL.addEventListener("mousedown",
+//             function () {
+//                 nav.box.scrollBy(-20,0)
+//             }
+//         )
+//     }
+// }
+
+
+//  nav.box.scrollBy(20,0)
 const nav = {
     box : document.getElementById("pagesBtnsBox"),
     goR : document.getElementById("GoRight"),
     goL : document.getElementById("Goleft"),
+    holdR : undefined ,
+    holdL : undefined ,
     runder : function(){
 
         this.goR.addEventListener("mousedown",
             function () {
-                nav.box.scrollBy(20,0)
+                nav.holdR=setInterval(
+                    function () {
+                        nav.box.scrollBy(50,0)
+                    },
+                    0
+                )
             }
+        );
+
+        this.goR.addEventListener("mouseup",
+            function () {
+                clearInterval(nav.holdR)
+            },
+            0
+        );
+
+        this.goL.addEventListener("mousedown",
+            function () {
+                nav.holdL=setInterval(
+                    function () {
+                        nav.box.scrollBy(-50,0)
+                    },
+                    0
+                )
+            }
+        );
+
+        this.goL.addEventListener("mouseup",
+            function () {
+                clearInterval(nav.holdL)
+            },
+            0
         );
 
 
 
 
-        this.goL.addEventListener("mousedown",
-            function () {
-                nav.box.scrollBy(-20,0)
-            }
-        )
+
+
     }
 }
+
+
 
 
 
