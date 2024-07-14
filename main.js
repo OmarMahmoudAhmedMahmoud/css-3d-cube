90646bfca3db81a656c2b5f0d6d549220a914582
Gman <omar5mahmoud3ahmed6@gmail.com>
2024-07-15 00:05:45 +0200

    add 'svg arrow icon' , make basic css style , compitle nav funtion ,              make main element changes contant when nav page changes

--- main.js
+++ main.js
@@ -1,83 +1,107 @@
-// const nav = {
-//     box : document.getElementById("pagesBtnsBox"),
-//     goR : document.getElementById("GoRight"),
-//     goL : document.getElementById("Goleft"),
-//     runder : function(){
-
-//         this.goR.addEventListener("mousedown",
-//             function () {
-//                 nav.box.scrollBy(20,0)
-//             }
-//         );
-
-
-
-
-//         this.goL.addEventListener("mousedown",
-//             function () {
-//                 nav.box.scrollBy(-20,0)
-//             }
-//         )
-//     }
-// }
-
-
-//  nav.box.scrollBy(20,0)
 const nav = {
+
+    routing:{
+        mainBox : document.getElementById("_3dBox"),
+        currnt : [document.getElementById("3d-tri"),
+            'tri'
+        ] ,
+        pages : {
+            cube :[document.getElementById("3d-cube"),
+                'cube'
+            ],
+            tri : [document.getElementById("3d-tri"),
+                'tri'
+            ]
+        },
+        loadPage: function(){
+            this.mainBox.innerHTML= this.currnt[1];
+   
+        },
+        render: function () {
+            this.currnt[0].style.backgroundColor="green";
+            this.loadPage()
+            for (const key in this.pages) {
+
+
+                this.pages[key][0].addEventListener("click",() => {
+                    if (this.currnt != this.pages[key][0]) {
+                        this.currnt[0].style.backgroundColor="transparent";
+                        this.currnt=this.pages[key];
+                        this.currnt[0].style.backgroundColor="green";
+                        this.loadPage()
+                    }
+                });
+
+                this.pages[key][0].addEventListener("mouseenter",() => {
+                    if (this.currnt[0] == this.pages[key][0]) {
+                        return
+                    }
+                    this.pages[key][0].style.backgroundColor="white";
+                    this.pages[key][0].style.color="black";
+                });
+
+                this.pages[key][0].addEventListener("mouseleave",() => {
+                    if (this.currnt[0] == this.pages[key][0]) {
+                        return
+                    }
+                    this.pages[key][0].style.backgroundColor="transparent";
+                    this.pages[key][0].style.color="white";
+                })
+            }
+        }
+    }
+}
+
+
+nav.style = {
     box : document.getElementById("pagesBtnsBox"),
     goR : document.getElementById("GoRight"),
     goL : document.getElementById("Goleft"),
     holdR : undefined ,
     holdL : undefined ,
-    runder : function(){
-
+    render : function(){
         this.goR.addEventListener("mousedown",
-            function () {
-                nav.holdR=setInterval(
-                    function () {
-                        nav.box.scrollBy(50,0)
+            ()=> {
+                this.holdR=setInterval(
+                    ()=>{
+                        nav.style.box.scrollBy(50,0)
                     },
                     0
                 )
             }
         );
-
         this.goR.addEventListener("mouseup",
-            function () {
-                clearInterval(nav.holdR)
+            ()=>{
+                clearInterval(this.holdR)
             },
             0
         );
-
         this.goL.addEventListener("mousedown",
-            function () {
-                nav.holdL=setInterval(
-                    function () {
-                        nav.box.scrollBy(-50,0)
+            () =>{
+                this.holdL=setInterval(
+                    ()=> {
+                        nav.style.box.scrollBy(-50,0)
                     },
                     0
                 )
             }
         );
-
         this.goL.addEventListener("mouseup",
-            function () {
-                clearInterval(nav.holdL)
+            ()=>{
+                clearInterval(this.holdL)
             },
             0
         );
-
-
-
-
-
-
     }
+}
+
+function runder() {
+    nav.style.render();
+    nav.routing.render();
 }
 
 
 
-
-
-nav.runder()
-
+document.body.onload=()=>{
+    runder()
+}
